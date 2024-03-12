0f38d673feb6f7fa71432a7f84c63524330fe469
DeveshTanwar123 <88023668+DeveshTanwar123@users.noreply.github.com>
2024-03-12 17:13:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+import Login from './Component/Login';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <p className="title">Registration Form</p>
+
+      <form className="App">
+        <input type="text" />
+        <input type="email" />
+        <input type="password" />
+        <input type={'submit'} />
+      </form>
+    </div>
+  );
+}
